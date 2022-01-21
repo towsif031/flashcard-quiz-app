9c23e45fdfeb202d0a9a6c8fbbdee0b5fa65d6bb
Towsif Ahmed Omi <towsif031@gmail.com>
2022-01-21 23:55:18 +0600

    flip flashcards.

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
+
 const Flashcard = ({ flashcard }) => {
-	return <div>{flashcard.question}</div>;
+	const [flip, setFlip] = useState(false);
+
+	return (
+		<div onClick={() => setFlip(!flip)}>
+			{flip ? flashcard.answer : flashcard.question}
+		</div>
+	);
 };
 
 export default Flashcard;
