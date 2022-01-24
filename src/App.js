44135962cab19089c4e46a06b411ca0617b24048
Towsif Ahmed Omi <towsif031@gmail.com>
2022-01-24 22:53:51 +0600

    fixed card width.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,11 @@ function App() {
 		return textArea.value;
 	}
 
-	return <FlashcardList flashcards={flashcards} />;
+	return (
+		<div className="container">
+			<FlashcardList flashcards={flashcards} />
+		</div>
+	);
 }
 
 const SAMPLE_FLASHCARDS = [
