408e94c88e1b71825d125aa9589545996d0a3f22
Towsif Ahmed Omi <towsif031@gmail.com>
2022-01-24 22:44:34 +0600

    decode html characters for rendering.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,16 @@ function App() {
 		axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
 			setFlashcards(
 				res.data.results.map((questionItem, index) => {
-					const answer = questionItem.correct_answer;
-					const options = [...questionItem.incorrect_answers, answer];
+					const answer = decodeString(questionItem.correct_answer);
+					const options = [
+						...questionItem.incorrect_answers.map((ans) =>
+							decodeString(ans)
+						),
+						answer
+					];
 					return {
 						id: `${index}-${Date.now()}`,
-						question: questionItem.question,
+						question: decodeString(questionItem.question),
 						answer: answer,
 						options: options.sort(() => Math.random() - 0.5)
 					};
@@ -22,6 +27,12 @@ function App() {
 			);
 		});
 	}, []);
+
+	function decodeString(str) {
+		const textArea = document.createElement('textarea');
+		textArea.innerHTML = str;
+		return textArea.value;
+	}
 
 	return <FlashcardList flashcards={flashcards} />;
 }
