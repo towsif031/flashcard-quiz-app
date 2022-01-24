6eedbf94112c9f223c18a4b96dd6f224c64502f0
Towsif Ahmed Omi <towsif031@gmail.com>
2022-01-24 22:38:10 +0600

    get data from Open Trivia Database API.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
+import axios from 'axios';
 
 function App() {
 	const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+	useEffect(() => {
+		axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
+			setFlashcards(
+				res.data.results.map((questionItem, index) => {
+					const answer = questionItem.correct_answer;
+					const options = [...questionItem.incorrect_answers, answer];
+					return {
+						id: `${index}-${Date.now()}`,
+						question: questionItem.question,
+						answer: answer,
+						options: options.sort(() => Math.random() - 0.5)
+					};
+				})
+			);
+		});
+	}, []);
 
 	return <FlashcardList flashcards={flashcards} />;
 }
