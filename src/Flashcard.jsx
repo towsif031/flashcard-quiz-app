0182ca8f30413e895e85f5aa5b8b89e61579f27a
Towsif Ahmed Omi <towsif031@gmail.com>
2022-01-25 23:56:07 +0600

    set card height.

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Flashcard = ({ flashcard }) => {
 	const [flip, setFlip] = useState(false);
+	const [height, setHeight] = useState('initial');
+
+	const frontEl = useRef();
+	const backEl = useRef();
+
+	function setMaxHeight() {
+		const frontHeight = frontEl.current.getBoundingClientRect().height;
+		const backHeight = backEl.current.getBoundingClientRect().height;
+		setHeight(Math.max(frontHeight, backHeight, 100));
+	}
+
+	useEffect(setMaxHeight, [
+		flashcard.question,
+		flashcard.answer,
+		flashcard.options
+	]);
 
 	return (
 		<div
 			className={`card ${flip ? 'flip' : ''}`}
+			style={{ height: height }}
 			onClick={() => setFlip(!flip)}
 		>
-			<div className="front">
+			<div className="front" ref={frontEl}>
 				{flashcard.question}
 				<div className="flashcard-options">
 					{flashcard.options.map((option) => {
@@ -16,7 +33,9 @@ const Flashcard = ({ flashcard }) => {
 					})}
 				</div>
 			</div>
-			<div className="back">{flashcard.answer}</div>
+			<div className="back" ref={backEl}>
+				{flashcard.answer}
+			</div>
 		</div>
 	);
 };
